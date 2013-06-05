657d4b8859ac522261c3c4a80da63588a736388f
Zelgiuz <anwebsa91@gmail.com>
2013-06-05 15:46:45 -0400

    fixed a bug where if you entered the correct user information into the register form it would log you in or if you entered the correct information with a current username and a different matching password it would log you in as nobody and show you the previous users information.

--- create_user_handler.php
+++ create_user_handler.php
@@ -15,15 +15,23 @@ function create_user($db){//Grab the arguments for the function from the post
     try {
         
  
-    
+      $sql= $db->prepare("SELECT * FROM `Accounts` WHERE `Username`= :Username");
+     
+      $sql->execute(array(':Username' => $sn));
+      if($row=$sql->fetch()){
+        if ($row!=NULL){
+          echo('<h3 class="warning"> Username already exists please try another</h3>');
+          return 0;
+        }  
+      }
 
 
       //If the username doesn't already exist  
       //hash the password for storage and create the sql command for creating a user 
       $pass=sha1($pw.SYS_SALT);
-      $sql= $db->prepare("INSERT INTO `Accounts`(`Username`, `Password`) VALUES ( :Username , :Password)");
+      $stmt= $db->prepare("INSERT INTO `Accounts`(`Username`, `Password`) VALUES ( :Username , :Password)");
      
-      $sql->execute(array(':Username' => $sn,':Password'=>$pass));
+      $stmt->execute(array(':Username' => $sn,':Password'=>$pass));
       login($db);
     //If the username already exists
 
